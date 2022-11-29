0bb3e462123883ffda58f96339e25eaaf6e1ee45
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2022-11-29 17:30:17 +0100

    Global Conventions

--- src/Persistence/BogusDbContext.cs
+++ src/Persistence/BogusDbContext.cs
@@ -25,6 +25,14 @@ public class BogusDbContext : DbContext
         });
     }
 
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        // All decimals should have 2 digits after the comma
+        configurationBuilder.Properties<decimal>().HavePrecision(18, 2);
+        // Max Length of a NVARCHAR that can be indexed
+        configurationBuilder.Properties<string>().HaveMaxLength(4_000);
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
