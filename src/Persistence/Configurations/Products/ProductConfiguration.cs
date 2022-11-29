46d66edf8e0f119f92e8c6599c28df76887f8c98
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2022-11-29 17:59:30 +0100

    Rename Price_Value column to Price

--- src/Persistence/Configurations/Products/ProductConfiguration.cs
+++ src/Persistence/Configurations/Products/ProductConfiguration.cs
@@ -10,7 +10,10 @@ internal class ProductConfiguration : EntityConfiguration<Product>
     public override void Configure(EntityTypeBuilder<Product> builder)
     {
         base.Configure(builder);
-        builder.OwnsOne(x => x.Price).Property(x => x.Value);
+
+        builder.OwnsOne(x => x.Price)
+               .Property(x => x.Value)
+               .HasColumnName(nameof(Product.Price));
     }
 }
 
