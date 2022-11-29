f55cec58f2fc527d192668a36414831bed05d18e
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2022-11-29 17:37:20 +0100

    Introduce EntityConfiguration<T>

---
+++ src/Persistence/Configurations/EntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BogusStore.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BogusStore.Persistence.Configurations;
+
+internal class EntityConfiguration<T> : IEntityTypeConfiguration<T> where T : Entity
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        // All tables are singlular named and have the name of the Entity
+        builder.ToTable(typeof(T).Name);
+        // All entities can be soft deleted but are not by default
+        builder.Property(x => x.IsEnabled).IsRequired().HasDefaultValue(true).ValueGeneratedNever();
+        // Default SQL constraint for CreatedAt and UpdatedAt
+        builder.Property(x => x.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+        builder.Property(x => x.UpdatedAt).HasDefaultValueSql("GETUTCDATE()").IsConcurrencyToken();
+    }
+}
+
